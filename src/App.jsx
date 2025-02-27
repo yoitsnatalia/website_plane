33577c480aeb6e77ec143c008e8b4b290d15ee8a
Natalia Linn <natalial@stanford.edu>
2025-02-27 00:18:40 -0800

    first draft airplane background + name

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import seat from "./assets/scene.png"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="relative h-screen w-screen flex items-center text-white font-instrument">
+        <img
+          src={seat}
+          alt="Background"
+          className="absolute inset-0 w-screen object-cover"
+        />
+        <h1 className="text-8xl relative bottom-7 left-10 rotate-5">Natalia Linn</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
