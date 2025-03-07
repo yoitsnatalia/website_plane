70311a87ed8194188560000b951eb7e2d3cd917e
Natalia Linn <natalial@stanford.edu>
2025-03-06 23:25:31 -0800

    full passport animation | issue: unexpected hover on close

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,35 +3,61 @@ import seal from "../assets/seal.png"
 import { useState } from "react"
 
 
-
 function AboutMe() {
     const [isOpen, setIsOpen] = useState(false);
-    const controls = useAnimation();
-    const textControls = useAnimation();
     const [isAnimating, setIsAnimating] = useState(false);
 
+    const controls = useAnimation();
+    const textControls = useAnimation();
+    const idControls = useAnimation();
+
     const aboutVariants = {
-        initial: { top: "115%" },
-        hover: { top: "105%" },
-        click: async () => {
+        initial: { y: "110%" },
+        hover: { y: "90%", transition: { ease: "easeInOut"} },
+        open: async () => {
             if (isAnimating) return;
             setIsOpen(true);
             setIsAnimating(true);
 
-            controls.set({ transformOrigin: "left center"});
+            await Promise.all([
+                controls.start({ y: "-100%", transition: { duration: 1.2, ease: "easeInOut" }}),
+                idControls.start({ y: "-100%", transition: { duration: 1.2, ease: "easeInOut" }})
+            ]);
 
-            await controls.start({ y: "-151.5%", x: "50%", scale: 1.5, rotate: 90, transition: { duration: 1 }});
+            controls.set({ transformOrigin: "left center"});
+            idControls.set({ transformOrigin: "left center" });
+
+            await Promise.all([
+                controls.start({ y: "-151.5%", x: "50%", scale: 1.5, rotate: 90, transition: { duration: 0.5, ease: "easeInOut" }}),
+                idControls.start({ y: "-151.5%", x: "50%", scale: 1.5, rotate: 90, transition: { duration: 0.5, ease: "easeInOut" }})
+            ]);
             
             await Promise.all([
-                //controls.start({ y: "-151.5%", transition: { duration: 1 }}),
-                controls.start({ rotateY: 180, transition: { duration: 1 }}),
-                textControls.start({ opacity: 0, transition: { duration: 0.3 } })
+                controls.start({ rotateY: 180, transition: { duration: 0.5, ease: "easeInOut" }}),
+                textControls.start({ opacity: 0, transition: { duration: 0.3, ease: "easeInOut" } })
             ]);
 
             setIsAnimating(false);
         },
-    };
+        close: async () => {
+            if (isAnimating) return;
+            setIsAnimating(true);
 
+            await Promise.all([
+                controls.start({ rotateY: 0, transition: { duration: 0.5, ease: "easeInOut" }}),
+                textControls.start({ opacity: 100, transition: { duration: 0.3, ease: "easeInOut" } })
+            ]);
+
+            await Promise.all([
+                controls.start({ y: 0, x: 0, scale: 1, rotate: 0, transition: { duration: 0.5, ease: "easeInOut" } }),
+                idControls.start({ y: 0, x: 0, scale: 1, rotate: 0, transition: { duration: 0.5, ease: "easeInOut" } })
+            ]);
+
+            setIsAnimating(false);
+            setIsOpen(false);
+        }
+    };
+    
     return (
         <div className="fixed flex inset-0 items-center justify-center pointer-events-none z-49">
             <div className="relative w-screen h-screen">
@@ -39,32 +65,41 @@ function AboutMe() {
                 <motion.div
                     id="passport"
                     variants={aboutVariants}
-                    style={{ transformOrigin: "top center" }}
                     initial="initial"
                     whileHover={!isOpen ? "hover" : undefined}
-                    animate={controls}
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-auto cursor-pointer text-amber-200 flex flex-col items-center justify-between p-15 h-148 w-87 rounded-2xl bg-blue-950"
-                    onClick={ () => {
-                        aboutVariants.click(); 
-                    }}
-                    >
-                        <motion.h1 animate={textControls} className="text-5xl">PASSPORT</motion.h1>
-                        <motion.img
-                            src={seal}
-                            alt="seal icon"
-                            animate={textControls}
-                            className="size-50 items-center"
-                        />
-                        <motion.div animate={textControls} className="justify-items-center">
-                            <h3 className="flex text-3xl">United States</h3>
-                            <h3 className="flex text-3xl">of America</h3>
-                        </motion.div>
-                </motion.div>
-                {/* ID */}
-                <motion.div
-                    className="absolute z-40 origin-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-auto opacity-10 rounded-2xl w-222 h-130 bg-black"
-                    >
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-auto">
 
+                    {/* Passport Cover */}
+                    <motion.div
+                        id="cover"
+                        variants={aboutVariants}
+                        className="absolute cursor-pointer z-50 h-148 w-87 rounded-2xl text-amber-200 flex flex-col items-center justify-between p-15 bg-blue-950"
+                        animate={controls}
+                        onClick={ () => {
+                            !isOpen ? aboutVariants.open() : aboutVariants.close();
+                        }}
+                        >
+                            <motion.h1 animate={textControls} className="text-5xl">PASSPORT</motion.h1>
+                            <motion.img
+                                src={seal}
+                                alt="seal icon"
+                                animate={textControls}
+                                className="size-50 items-center"
+                            />
+                            <motion.div animate={textControls} className="justify-items-center">
+                                <h3 className="flex text-3xl">United States</h3>
+                                <h3 className="flex text-3xl">of America</h3>
+                            </motion.div>
+                    </motion.div>
+
+                    {/* ID */}
+                    <motion.div
+                        id="id"
+                        variants={aboutVariants}
+                        className="flex z-10 rounded-2xl h-148 w-87 bg-indigo-50"
+                        animate={idControls}
+                        >
+                    </motion.div>
                 </motion.div>
             </div>
         </div>
