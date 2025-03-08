0a829c527252fa28f75089acd9cbfb529a4e2c1b
Natalia Linn <natalial@stanford.edu>
2025-03-07 23:12:02 -0800

    id content complete

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,8 @@
 import { motion, useAnimation } from "framer-motion";
 import seal from "../assets/seal.png"
 import { useState } from "react"
+import headshot from "../assets/headshot1.png"
+import airplaneVector from "../assets/airplaneVector.png"
 
 
 function AboutMe() {
@@ -99,26 +101,45 @@ function AboutMe() {
                         className="flex z-10 rounded-2xl h-148 w-87 relative bg-indigo-50 items-center justify-center"
                         animate={idControls}
                         >
-                        <div className="absolute w-148 h-87 rounded-2xl bg-amber-300 transform -rotate-90 origin-center flex items-center justify-center">
-                            <div className="grid grid-cols-2 text-black">
-                                <div></div>
-                                <div className="">
+                        <div className="absolute w-148 h-87 rounded-2xl transform -rotate-90 origin-center flex justify-start">
+                            <div className="m-10 text-indigo-950 items-center">   
+                                <div className="flex flex-inline">
                                     <div>
-                                        <h3 className="text-sm">Name</h3>
-                                        <h1 className="text-3xl">Natalia Linn</h1>
+                                        <img
+                                            src={headshot}
+                                            alt="Natalia Linn"
+                                            className="size-60 object-contain"
+                                        />
                                     </div>
-                                    <div className="flex flex-inline">
-                                        <div className="">
-                                            <h3 className="text-sm">Class</h3>
-                                            <h1 className="text-3xl">2027</h1>
+                                    <div className="flex flex-col ml-5">
+                                        <div className="mb-5">
+                                            <h3 className="text-xs">Name</h3>
+                                            <h1 className="text-4xl">Natalia Linn</h1>
                                         </div>
-                                        <div className="">
-                                            <h3 className="text-sm">School</h3>
-                                            <h1 className="text-3xl text-nowrap">Stanford University</h1>
+                                        <div className="flex flex-inline mb-2">
+                                            <div className="mr-5">
+                                                <h3 className="text-xs">Class</h3>
+                                                <h1 className="text-2xl">'27</h1>
+                                            </div>
+                                            <div>
+                                                <h3 className="text-xs">School</h3>
+                                                <h1 className="text-2xl text-nowrap">Stanford University</h1>
+                                            </div>
+                                        </div>
+                                        <div className="mb-2">
+                                            <h3 className="text-xs">Major</h3>
+                                            <h1 className="text-2xl">Computer Science</h1>
+                                        </div>
+                                        <div>
+                                            <h3 className="text-xs">Home</h3>
+                                            <h1 className="text-2xl">Orange County, CA</h1>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                                <div className="flex place-self-center text-xs mt-6 ml-10">
+                                    <p>Hey! Thanks for checking out my website! </p>
+                                </div>
+                            </div> 
                         </div>
                     </motion.div>
                 </motion.div>
